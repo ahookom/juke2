45ef5e03ba46b95ebea192feefff81e61f8fbc66
mandi-m <mmeidlinger@yahoo.com>
2017-03-29 13:03:35 -0400

    subroutes to do

--- browser/react/components/Artist.js
+++ browser/react/components/Artist.js
@@ -1,35 +1,51 @@
 import React from 'react';
 import { Link } from 'react-router';
 import axios from 'axios';
+import Albums from './Albums';
+import Songs from './Songs';
+import { convertAlbum, convertAlbums, skip } from '../utils';
 
 export default class Artist extends React.Component {
   constructor(props) {
     super(props)
-
-    this.artistId = '1';
+    this.artistId = +this.props.routeParams.artistId;
     this.albums=[];
     this.songs=[];
-    this.artist=this.props.artists.filter(artist=>{artist.id===this.artistId});
-    console.log(this.artist);
+    this.artist=this.props.artists.filter(function(artist){
+      return artist.id === this.artistId}, this)[0];
   }
   componentDidMount(){
 
-    // axios.get(`/api/${this.artistId}/albums`)
-    // .then(res=>res.data)
-    // .then(albums=>this.albums=albums)
-    // .catch()
-    // axios.get(`/api/${this.artistId}/songs`)
-    // .then(res=>res.data)
-    // .then(songs=>this.songs=songs)
-    // .catch()
+    axios.get(`/api/artists/${this.artistId}/albums`)
+    .then(res=>res.data)
+    .then(albums=>{
+      this.albums=convertAlbums(albums);
+      this.setState({albums});
+    })
+    .catch()
+
+    axios.get(`/api/artists/${this.artistId}/songs`)
+    .then(res=>res.data)
+    .then(songs=>{
+      this.songs=songs;
+      this.setState({songs});
+    })
+    .catch()
   }
+
   render() {
     return (
       <div>
       <h1>HELLO WORLD</h1>
-        <h3>{this.artist.name}</h3>
-        <h4>{JSON.stringify(this.albums[0])}</h4>
-        <h4>{JSON.stringify(this.songs[0])}</h4>
+        <h3>{this.artist.name? this.artist.name : null}</h3>
+        {this.albums.length ? <Albums albums={this.albums}/> : null }
+        {this.songs.length ?
+        <Songs
+        songs={this.songs}
+        currentSong={this.props.currentSong}
+        isPlaying={this.props.isPlaying}
+        toggleOne={this.props.toggleOne} />
+        : null }
       </div>
     )
   }
